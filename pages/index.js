c09ce7e6535da6c4811ad9793009f753698c96eb
Nina Busz <buss.nina@googlemail.com>
2023-03-07 15:38:55 +0100

    fetch art pieces with swr

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,9 @@
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then((r) => r.json());
+
 export default function HomePage() {
+  const { data } = useSWR(`https://example-apis.vercel.app/api/art`, fetcher);
   return (
     <div>
       <h1>Hello from Next.js</h1>
